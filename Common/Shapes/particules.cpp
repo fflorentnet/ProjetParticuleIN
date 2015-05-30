02ca2504c501ba51457661bfc2b767a4e7e290be
Benjamin <bendescamps@wanadoo.fr>
2015-05-31 01:56:53 +0200

    Even more particles ! :D

--- Common/Shapes/particules.cpp
+++ Common/Shapes/particules.cpp
@@ -1,60 +1,93 @@
 #include "particules.h"
 
-GLfloat positionsParticules[] =
-{
-    0.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    2.0f, 0.0f, 0.0f,
-    3.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 2.0f, 0.0f,
-    0.0f, 3.0f, 0.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 2.0f,
-    0.0f, 0.0f, 3.0f
-};
-
-GLfloat g_TabColors3[] =
-{
-    1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 1.0f
-};
-
-GLfloat g_TabLifes[] =
-{
-    1.0f,
-    1.0f,
-    0.0f,
-    1.0f,
-    1.0f,
-    0.0f,
-    0.0f,
-    0.0f,
-    0.0f,
-    0.0f
-};
-
 
 
 Particules::Particules()
 {
     life = 0.0f;
+    float currentVal = 0.0f;
+    int counter = 0;
+    for( GLuint i = 0; i < NB_PARTICULES*POINTS; i += POINTS )
+    {
+
+        m_tabVertices[i] = currentVal;
+        m_tabVertices[i+1] = 0.0f;
+        m_tabVertices[i+2] = 0.0f;
+
+        m_tabVertices[i+3] = 0.0f;
+        m_tabVertices[i+4] = currentVal;
+        m_tabVertices[i+5] = 0.0f;
+
+        m_tabVertices[i+6] = 0.0f;
+        m_tabVertices[i+7] = 0.0f;
+        m_tabVertices[i+8] = currentVal;
+
+        m_tabVertices[i+9] = currentVal;
+        m_tabVertices[i+10] = currentVal;
+        m_tabVertices[i+11] = 0.0f;
+
+        m_tabVertices[i+12] = 0.0f;
+        m_tabVertices[i+13] = currentVal;
+        m_tabVertices[i+14] = currentVal;
+
+        m_tabVertices[i+15] = currentVal;
+        m_tabVertices[i+16] = 0.0f;
+        m_tabVertices[i+17] = currentVal;
+
+        m_tabVertices[i+18] = currentVal;
+        m_tabVertices[i+19] = currentVal;
+        m_tabVertices[i+20] = currentVal;
+
+
+        m_tabColors[i] = 0.0f;
+        m_tabColors[i+1] = 0.0f;
+        m_tabColors[i+2] = 0.0f;
+
+        m_tabColors[i+3] = 0.0f;
+        m_tabColors[i+4] = 0.0f;
+        m_tabColors[i+5] = 0.0f;
+
+        m_tabColors[i+6] = 0.0f;
+        m_tabColors[i+7] = 0.0f;
+        m_tabColors[i+8] = 0.0f;
+
+        m_tabColors[i+9] = 0.0f;
+        m_tabColors[i+10] = 0.0f;
+        m_tabColors[i+11] = 0.0f;
+
+        m_tabColors[i+12] = 0.0f;
+        m_tabColors[i+13] = 0.0f;
+        m_tabColors[i+14] = 0.0f;
+
+        m_tabColors[i+15] = 0.0f;
+        m_tabColors[i+16] = 0.0f;
+        m_tabColors[i+17] = 0.0f;
+
+        m_tabColors[i+18] = 0.0f;
+        m_tabColors[i+19] = 0.0f;
+        m_tabColors[i+20] = 0.0f;
+
+
+
+        m_tabLives[counter*7] = 0.0f;
+        m_tabLives[counter*7+1] = 0.0f;
+        m_tabLives[counter*7+2] = 0.0f;
+        m_tabLives[counter*7+3] = 0.0f;
+        m_tabLives[counter*7+4] = 0.0f;
+        m_tabLives[counter*7+5] = 0.0f;
+        m_tabLives[counter*7+6] = 0.0f;
+
+        counter++;
+        currentVal += 0.2f;
+    }
 }
 void
 Particules::update()
 {
     life+= 0.1f; // contient la vie de l'ensemble / du phenomene
-    for( GLuint i = 0; i < 10; ++i )
+    for( GLuint i = 0; i < NB_PARTICULES * 7; ++i )
     {
-        g_TabLifes[i] = life; // Chaque particule a sa variable de vie associée
+        m_tabLives[i] = life; // Chaque particule a sa variable de vie associée
     }
 }
 
@@ -76,19 +109,19 @@ Particules::drawShape()
 {
     GLint var1 = glGetAttribLocation( m_Framework->getCurrentShaderId(), "position" );
     glEnableVertexAttribArray( var1 );
-    glVertexAttribPointer( var1, 3, GL_FLOAT, GL_FALSE, 0, positionsParticules );
+    glVertexAttribPointer( var1, 3, GL_FLOAT, GL_FALSE, 0, m_tabVertices );
     GLint var2 = glGetAttribLocation( m_Framework->getCurrentShaderId(), "color" );
     glEnableVertexAttribArray( var2 );
-    glVertexAttribPointer( var2, 3, GL_FLOAT, GL_FALSE, 0, g_TabColors3 );
+    glVertexAttribPointer( var2, 3, GL_FLOAT, GL_FALSE, 0, m_tabColors );
 
     // On passe le tableau contenant 1 variable de vie / sommet
     GLint var3 = glGetAttribLocation( m_Framework->getCurrentShaderId(), "life" );
     glEnableVertexAttribArray( var3 );
-    glVertexAttribPointer( var3, 1, GL_FLOAT, GL_FALSE, 0, g_TabLifes );
+    glVertexAttribPointer( var3, 1, GL_FLOAT, GL_FALSE, 0, m_tabLives );
 
     glPointSize(10);
     //glDrawElements( GL_POINTS, 10, GL_UNSIGNED_INT, positionsParticules);
-    glDrawArrays( GL_POINTS, 0, 10 );
+    glDrawArrays( GL_POINTS, 0, NB_PARTICULES * 7 );
 
     glDisableVertexAttribArray( var1 );
     glDisableVertexAttribArray( var2 );
--- Common/Shapes/particules.h
+++ Common/Shapes/particules.h
@@ -3,6 +3,9 @@
 
 #include "Object3D.h"
 
+#define POINTS 21
+#define NB_PARTICULES 50
+
 class Particules : public Object3D
 {
 public:
@@ -12,9 +15,13 @@ public:
 
 protected:
     void drawShape();
+    GLfloat m_tabVertices[NB_PARTICULES*POINTS];
+    GLfloat m_tabColors[NB_PARTICULES*POINTS];
+    GLfloat m_tabLives[NB_PARTICULES*7];
 
 private:
     float life;
+
 };
 
 #endif // PARTICULES_H
